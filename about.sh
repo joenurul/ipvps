de3bb9b756613a216539ed80b26d58fbffd57d6a
Dimas1441 <89393904+Dimas1441@users.noreply.github.com>
2021-08-24 16:48:37 +0700

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+clear
+echo -e "=================================================" | lolcat
+echo -e "#                   BRODY                     #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# For Debian 9 & Debian 10 64 bit               #" | lolcat
+echo -e "# For Ubuntu 18.04 & Ubuntu 20.04 64 bit        #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "# xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx#" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                    BRODY                     #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#                                               #" | lolcat
+echo -e "#-----------------------------------------------#" | lolcat
+echo -e "                     BRODY                      #" | lolcat
+echo -e "=================================================" | lolcat
